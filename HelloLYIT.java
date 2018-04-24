521db49e8ee0261b60f510479ac04953495b1109
John Burke <burke_j@hotmail.com>
2018-04-24 22:03:49 +0100

    Added HelloLYIT.java

---
+++ HelloLYIT.java
@@ -0,0 +1,10 @@
+#This is a basic file to give instructions as to the content of the directory
+#and how the content should be used
+# created by John Burke 
+# April 2018
+public class HelloLYIT {
+    public static void main(String[] args) {
+        // Prints "Hello, LYIT from JB" as output
+        System.out.println("Hello, LYIT from JB");
+    }
+}
